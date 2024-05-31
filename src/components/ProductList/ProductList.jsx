de69c205eb819bbfe4f997b876c4c8f6812a33aa
DN82 <pulya827@gmail.com>
2024-05-31 15:04:36 +0700

    Продукция

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,12 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Пельмени вареные куриные', price: 70, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Пельмени жареные куриные', price: 70, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Пельмени вареные свинина', price: 75, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Пельмени жареные свинина', price: 75, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Пельмени вареные говядина-свинина', price: 80, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Пельмени жареные говядина-свинина', price: 80, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
